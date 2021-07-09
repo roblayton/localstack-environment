164edd42d19f6da4427c2acfea717fc48853409e
Rob Layton <rob@codeboxsystems.com>
2021-07-09 21:19:32 +0200

    Updated schem

--- constants.py
+++ constants.py
@@ -7,35 +7,53 @@ cfg = config[os.environ['ENVIRONMENT']]
 LAMBDA_ENV_VARS = quote('{SQS_ENDPOINT=%s,USE_SSL=%s,ADYN_AWS_SECRET_ACCESS_KEY=%s,ADYN_AWS_ACCESS_KEY=%s,AWS_REGION=%s}' % (cfg.LOCALSTACK_ENDPOINT, cfg.USE_SSL, cfg.ADYN_AWS_SECRET_ACCESS_KEY, cfg.ADYN_AWS_ACCESS_KEY, cfg.REGION))
 
 LAMBDAS = [{
-    'package': 'shipment-service',
-    'path': '/home/rob/repos/shipment-service/',
-    'name': 'ShipmentService',
+    # 'package': 'referral-hero-worker',
+    # 'path': '/home/rob/repos/referral-hero-worker/',
+    # 'name': 'ReferralHeroWorker',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
+# }, {
+    # 'package': 'hubspot-creator',
+    # 'path': '/home/rob/repos/hubspot-creator/',
+    # 'name': 'HubspotCreator',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
+# }, {
+    'package': 'hubspot-adopter',
+    'path': '/home/rob/repos/hubspot-adopter/',
+    'name': 'HubspotAdopter',
     'role': 'LambdaExecutionRole',
     'env_vars': None
 }, {
-    'package': 'usps-tracker',
-    'path': '/home/rob/repos/usps-tracker/',
-    'name': 'USPSTracker',
-    'role': 'LambdaExecutionRole',
-    'env_vars': None
-}, {
-    'package': 'usps-mocker',
-    'path': '/home/rob/repos/usps-mocker/',
-    'name': 'USPSMocker',
-    'role': 'LambdaExecutionRole',
-    'env_vars': None
-}, {
+    # 'package': 'shipment-service',
+    # 'path': '/home/rob/repos/shipment-service/',
+    # 'name': 'ShipmentService',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
+# }, {
+    # 'package': 'usps-tracker',
+    # 'path': '/home/rob/repos/usps-tracker/',
+    # 'name': 'USPSTracker',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
+# }, {
+    # 'package': 'usps-mocker',
+    # 'path': '/home/rob/repos/usps-mocker/',
+    # 'name': 'USPSMocker',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
+# }, {
     'package': 'usps-checker',
     'path': '/home/rob/repos/usps-checker/',
     'name': 'USPSChecker',
     'role': 'LambdaExecutionRole',
     'env_vars': None
-# }, {
-    # 'package': 'order-service',
-    # 'path': '/home/rob/repos/order-service/',
-    # 'name': 'OrderService',
-    # 'role': 'LambdaExecutionRole',
-    # 'env_vars': None
+}, {
+    'package': 'order-service',
+    'path': '/home/rob/repos/order-service/',
+    'name': 'OrderService',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
 # }, {
     # 'package': 'auth-service',
     # 'path': '/home/rob/repos/auth-service/',
@@ -46,24 +64,42 @@ LAMBDAS = [{
     # 'env_vars': LAMBDA_ENV_VARS,
     # 'kickstart_body': 'Subject',
     # 'kickstart_attributes': 'file://mailer-attributes.json'
-# }, {
-    # 'package': 'address-service',
-    # 'path': '/home/rob/repos/address-service/',
-    # 'name': 'AddressService',
-    # 'role': 'LambdaExecutionRole',
-    # 'env_vars': None
+}, {
+    'package': 'behavior-worker',
+    'path': '/home/rob/repos/behavior-worker/',
+    'name': 'BehaviorWorker',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
+}, {
+    'package': 'jotform-worker',
+    'path': '/home/rob/repos/jotform-worker/',
+    'name': 'JotformWorker',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
+}, {
+    'package': 'medical-bio-service',
+    'path': '/home/rob/repos/medical-bio-service/',
+    'name': 'MedicalBioService',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
+}, {
+    'package': 'address-service',
+    'path': '/home/rob/repos/address-service/',
+    'name': 'AddressService',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
 }, {
     'package': 'customer-service',
     'path': '/home/rob/repos/customer-service/',
     'name': 'CustomerService',
     'role': 'LambdaExecutionRole',
     'env_vars': None
-# }, {
-    # 'package': 'email-service',
-    # 'path': '/home/rob/repos/email-service/',
-    # 'name': 'EmailService',
-    # 'role': 'LambdaExecutionRole',
-    # 'env_vars': None
+}, {
+    'package': 'email-service',
+    'path': '/home/rob/repos/email-service/',
+    'name': 'EmailService',
+    'role': 'LambdaExecutionRole',
+    'env_vars': None
 # }, {
     # 'package': 'email-worker',
     # 'path': '/home/rob/repos/email-worker/',
@@ -94,24 +130,24 @@ LAMBDAS = [{
     'name': 'AlliedChecker',
     'role': 'LambdaExecutionRole',
     'env_vars': None
-}, {
-    'package': 'allied-tracker',
-    'path': '/home/rob/repos/allied-tracker/',
-    'name': 'AlliedTracker',
-    'role': 'LambdaExecutionRole',
-    'env_vars': None
+# }, {
+    # 'package': 'allied-tracker',
+    # 'path': '/home/rob/repos/allied-tracker/',
+    # 'name': 'AlliedTracker',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
 # }, {
     # 'package': 'allied-resetter',
     # 'path': '/home/rob/repos/allied-resetter/',
     # 'name': 'AlliedResetter',
     # 'role': 'LambdaExecutionRole',
     # 'env_vars': None
-}, {
-    'package': 'allied-mocker',
-    'path': '/home/rob/repos/allied-mocker/',
-    'name': 'AlliedMocker',
-    'role': 'LambdaExecutionRole',
-    'env_vars': None
+# }, {
+    # 'package': 'allied-mocker',
+    # 'path': '/home/rob/repos/allied-mocker/',
+    # 'name': 'AlliedMocker',
+    # 'role': 'LambdaExecutionRole',
+    # 'env_vars': None
 # }, {
     # 'package': 'activation-service',
     # 'path': '/home/rob/repos/activation-service/',
--- schema.sql
+++ schema.sql
@@ -4,45 +4,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE GenotypeValues CASCADE;
 DROP TABLE HormoneValues CASCADE;
-DROP TABLE Whereabouts_Customers CASCADE;
-DROP TABLE ShippingAddresses_Customers CASCADE;
 DROP TABLE Shipments CASCADE;
 DROP TABLE OrderHistory CASCADE;
 DROP TABLE Orders CASCADE;
-DROP TABLE Customers CASCADE;
-DROP TABLE Whereabouts CASCADE;
+DROP TABLE MedicalBioSubmissions CASCADE;
 DROP TABLE ShippingAddresses CASCADE;
+DROP TABLE Customers CASCADE;
 DROP TABLE Genotypes CASCADE;
 DROP TABLE Hormones CASCADE;
 
-CREATE TABLE IF NOT EXISTS Whereabouts (
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  zip VARCHAR(45) NOT NULL,
-  from_date TIMESTAMP NOT NULL,
-  to_date TIMESTAMP NOT NULL,
-  created_date TIMESTAMP NOT NULL,
-  modified_date TIMESTAMP,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS ShippingAddresses (
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  name VARCHAR(255) NOT NULL,
-  address1 VARCHAR(255) NOT NULL,
-  address2 VARCHAR(255),
-  city VARCHAR(45) NOT NULL,
-  state VARCHAR(2) NOT NULL,
-  zip VARCHAR(45) NOT NULL,
-  country VARCHAR(2) NOT NULL,
-  delivery_instructions TEXT,
-  created_date TIMESTAMP NOT NULL,
-  modified_date TIMESTAMP,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS Customers (
   id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  shipping_address_id uuid,
   email VARCHAR(80) UNIQUE NOT NULL,
   new_email VARCHAR(80) UNIQUE,
   phone VARCHAR(20),
@@ -60,50 +32,63 @@ CREATE TABLE IF NOT EXISTS Customers (
   u_code VARCHAR(255),
   u_code_used BOOLEAN DEFAULT True,
   role INTEGER NOT NULL DEFAULT 2,
+  -- 0 = admin, 1 = editor, 2 = user/customer, 3 = purchaser, 4 = guest
   status INTEGER NOT NULL DEFAULT 1,
+  age_confirmed BOOLEAN DEFAULT False,
+  residence_confirmed BOOLEAN DEFAULT False,
+  terms_confirmed BOOLEAN DEFAULT False,
   email_pref BOOLEAN NOT NULL DEFAULT True,
-  hubspot_vid VARCHAR(45),
+  referral_hero_id VARCHAR(45),
+  hubspot_id VARCHAR(45),
+  hubspot_list INTEGER DEFAULT 0,
   stripe_customer_id VARCHAR(45),
   steadymd_patient_id VARCHAR(45),
   active BOOLEAN NOT NULL DEFAULT True,
   created_date TIMESTAMP NOT NULL,
   modified_date TIMESTAMP,
   ip VARCHAR(45),
-  CONSTRAINT fk_shipping_address
-    FOREIGN KEY (shipping_address_id) 
-      REFERENCES ShippingAddresses(id)
-      ON DELETE CASCADE,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Whereabouts_Customers (
-  whereabouts_id uuid NOT NULL,
+CREATE TABLE IF NOT EXISTS MedicalBioSubmissions (
+  id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  jotform_id VARCHAR(45) NOT NULL,
+  jotform_form_id VARCHAR(45) NOT NULL,
   customer_id uuid NOT NULL,
-  CONSTRAINT fk_whereabouts
-    FOREIGN KEY (whereabouts_id) 
-      REFERENCES Whereabouts(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
+  ip VARCHAR(45),
+  completion INTEGER,
+  elapsed_time INTEGER,
+  created_date TIMESTAMP NOT NULL,
+  modified_date TIMESTAMP,
   CONSTRAINT fk_customer_id
     FOREIGN KEY (customer_id) 
       REFERENCES Customers(id)
       ON UPDATE CASCADE
-      ON DELETE CASCADE
+      ON DELETE CASCADE,
+  UNIQUE(customer_id, jotform_form_id),
+  PRIMARY KEY (id, customer_id, jotform_form_id)
 );
 
-CREATE TABLE IF NOT EXISTS ShippingAddresses_Customers (
-  shipping_address_id uuid NOT NULL,
-  customer_id uuid NOT NULL,
-  CONSTRAINT fk_shipping_address
-    FOREIGN KEY (shipping_address_id) 
-      REFERENCES ShippingAddresses(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS ShippingAddresses (
+  id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  customer_id uuid NOT NULL UNIQUE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  address1 VARCHAR(255) NOT NULL,
+  address2 VARCHAR(255),
+  city VARCHAR(45) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip VARCHAR(45) NOT NULL,
+  country VARCHAR(2) NOT NULL,
+  delivery_instructions TEXT,
+  created_date TIMESTAMP NOT NULL,
+  modified_date TIMESTAMP,
   CONSTRAINT fk_customer_id
     FOREIGN KEY (customer_id) 
       REFERENCES Customers(id)
       ON UPDATE CASCADE
-      ON DELETE CASCADE
+      ON DELETE CASCADE,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
@@ -111,6 +96,7 @@ CREATE TABLE IF NOT EXISTS Orders (
   customer_id uuid NOT NULL,
   stripe_invoice_item_id VARCHAR(45),
   activation_code VARCHAR(45),
+  activated BOOLEAN DEFAULT False,
   sku VARCHAR(12),
   package_unit VARCHAR(5),
   package_unit_quantity INTEGER,
